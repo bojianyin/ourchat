f934d655030e165f1a30eaa76d7be92949433af4
bojianyin <2316744840@qq.com>
2023-04-06 14:17:34 +0800

    优化断开链接时候 不能收到消息的问题

--- app/src/main/java/com/app/ourchat/OurApplication.kt
+++ app/src/main/java/com/app/ourchat/OurApplication.kt
@@ -6,13 +6,17 @@ import androidx.multidex.MultiDex
 import com.app.ourchat.utils.*
 import io.rong.imlib.RongCoreClient
 import io.rong.imlib.listener.OnReceiveMessageWrapperListener
+import io.rong.imlib.model.Conversation
 import io.rong.imlib.model.Message
 import io.rong.imlib.model.ReceivedProfile
 import io.rong.message.TextMessage
+import io.rong.push.PushEventListener
+import io.rong.push.PushType
 import io.rong.push.RongPushClient
+import io.rong.push.notification.PushNotificationMessage
 import io.rong.push.pushconfig.PushConfig
 
-class OurApplication : Application() {
+class OurApplication : Application(), PushEventListener {
 
     private val messageListener = object : OnReceiveMessageWrapperListener() {
         override fun onReceivedMessage(message: Message?, profile: ReceivedProfile?) {
@@ -32,10 +36,13 @@ class OurApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         appContext = this
+
         RongCoreClient.init(this, Constant.rongCloudAppKey,true)
         RongCoreClient.addOnReceiveMessageListener(messageListener)
+
         val build = PushConfig.Builder().enableRongPush(true).build()
         RongPushClient.setPushConfig(build)
+        RongPushClient.setPushEventListener(this)
         RongPushClient.init(this, Constant.rongCloudAppKey)
     }
 
@@ -46,6 +53,59 @@ class OurApplication : Application() {
 
     fun removeMessageListener(){
         RongCoreClient.removeOnReceiveMessageListener(messageListener)
+    }
+
+    override fun preNotificationMessageArrived(
+        context: Context?,
+        pushType: PushType?,
+        notificationMessage: PushNotificationMessage?
+    ): Boolean {
+        notificationMessage?.let {
+            val message = Message.obtain(
+                it.targetId,
+                Conversation.ConversationType.setValue(it.conversationType.value),
+                TextMessage(it.pushContent)
+            )
+            message.targetId = it.targetId
+            message.senderUserId = it.senderId
+            EventBusUtil.postMessage(MessageEvent(MessageEvent.msg_received_chat,message))
+        }
+
+        return false
+    }
+
+    override fun afterNotificationMessageArrived(
+        context: Context?,
+        pushType: PushType?,
+        notificationMessage: PushNotificationMessage?
+    ) {
+
+    }
+
+    override fun onNotificationMessageClicked(
+        context: Context?,
+        pushType: PushType?,
+        notificationMessage: PushNotificationMessage?
+    ): Boolean {
+
+        return false
+    }
+
+    override fun onThirdPartyPushState(pushType: PushType?, action: String?, resultCode: Long) {
+
+    }
+
+    override fun onTokenReceived(pushType: PushType?, token: String?) {
+
+    }
+
+    override fun onTokenReportResult(
+        reportType: PushType?,
+        code: Int,
+        finalType: PushType?,
+        finalToken: String?
+    ) {
+
     }
 
 }
